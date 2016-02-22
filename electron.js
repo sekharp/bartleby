3a6d0acd1fdf8453c604d609196d162ac292c77d
Steve Kinney <hello@stevekinney.net>
2016-02-22 06:50:39 -0700

    Adjust formatting

--- electron.js
+++ electron.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const electron         = require('electron');
+
 const app              = electron.app;
 const BrowserWindow    = electron.BrowserWindow;
 const emberAppLocation = `file://${__dirname}/dist/index.html`;
@@ -11,37 +12,26 @@ let mainWindow = null;
 electron.crashReporter.start();
 
 app.on('window-all-closed', function onWindowAllClosed() {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('ready', function onReady() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600
-    });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
 
-    delete mainWindow.module;
+  delete mainWindow.module;
 
-    // If you want to open up dev tools programmatically, call
-    // mainWindow.openDevTools();
+  mainWindow.loadURL(emberAppLocation);
 
-    // By default, we'll open the Ember App by directly going to the
-    // file system.
-    //
-    // Please ensure that you have set the locationType option in the
-    // config/environment.js file to 'hash'. For more information,
-    // please consult the ember-electron readme.
+  mainWindow.webContents.on('did-fail-load', () => {
     mainWindow.loadURL(emberAppLocation);
+  });
 
-    // If a loading operation goes wrong, we'll send Electron back to
-    // Ember App entry point
-    mainWindow.webContents.on('did-fail-load', () => {
-        mainWindow.loadURL(emberAppLocation);
-    });
-
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 });
