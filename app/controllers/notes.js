1adc2cb043da8ecb9519620ec240b3905e57309b
Sekhar Paladugu <sekharp@gmail.com>
2016-02-23 16:41:21 -0700

    got notes body and title saving

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   newNoteTitle: null,
+  newNoteBody: null,
 
   actions: {
     createNote() {
-        debugger
       this.store.createRecord('note', {
         id: this.get('newNoteTitle') + '.md',
-        body: this.get('newNoteBody')
+        content: this.get('newNoteBody')
       }).save().then(data => {
         this.transitionToRoute('notes.note', data);
       });
